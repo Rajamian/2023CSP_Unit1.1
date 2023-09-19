50077ba8d3cf216f76b610f59e2ff39e6fc742a7
Ryan <ryan.ajamian@vtsd.com>
2023-09-19 10:06:45 -0400

    Test File

---
+++ 1-1-1/1-1-2/CastingPractice.py
@@ -0,0 +1,16 @@
+Something = 1.9999999
+SomethingElse = 65
+um = ("uh")
+
+print("Intitial Values:")
+print("something:", Something)
+print("somethingElse:", SomethingElse)
+print("um", um)
+
+Something = chr(SomethingElse)
+
+
+print("Ending Values:")
+print("something:", Something)
+print("somethingElse:", SomethingElse)
+print("um", um)
